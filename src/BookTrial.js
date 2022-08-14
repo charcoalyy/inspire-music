9aa9c875d917a02c65736b7b06bb7b7501bb6864
Alina Zeng <alina.yzeng@gmail.com>
2022-08-13 22:45:46 -0400

    ADD google calendar events

--- src/BookTrial.js
+++ src/BookTrial.js
@@ -131,7 +131,7 @@ const BookTrial = () => {
                 </div>
                 <div className="lesson-form-section">
                     <label>Lesson time</label>
-                    <iframe src="https://calendar.google.com/calendar/embed?src=fhkbtcs80q9sjvok0m0m7n94tg%40group.calendar.google.com&ctz=America%2FToronto" style={{border: 0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+                    {/* <iframe src="https://calendar.google.com/calendar/embed?src=fhkbtcs80q9sjvok0m0m7n94tg%40group.calendar.google.com&ctz=America%2FToronto" style={{border: 0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe> */}
                     <Calendar />
                 </div>
                 <button className="button">Book lesson</button>
--- src/Calendar.js
+++ src/Calendar.js
@@ -1,49 +1,35 @@
 import { gapi } from 'gapi-script'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 const Calendar = () => {
 
     const [events, setEvents] = useState([]);
 
-    let apiKey = "AIzaSyBoRzqTgmRxEQ99C3X9RNF5JVCDO82n6nM"
-    let calendarID = "fhkbtcs80q9sjvok0m0m7n94tg@group.calendar.google.com"
-
     const getEvents = (calendarID, apiKey) => {
-        function initiate() {
-            gapi.client
-              .init({
-                apiKey: apiKey,
-                calendarID: calendarID
-              })
-        
-              .then(function () {
-                return gapi.client.request({
-                  path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
-                });
-              })
-        
-              .then(
-                (response) => {
-                  let events = response.result.items;
-                  return events;
-                },
-                function (err) {
-                  return [false, err];
-                }
-              );
-          }
-        
-          gapi.load("client", initiate);
+      function start() {
+        gapi.client.init({
+          'apiKey': apiKey,
+          'calendarID': calendarID
+        }).then(() => {
+          return gapi.client.request({'path': `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`})
+        }).then((response) => {
+          let tests = response.result.items
+          setEvents(tests)
+          return true
+        }, (err) => {
+          return err
+        })
+      }
+      gapi.load('client', start)
     }
 
+
     useEffect(() => {
-        const events = getEvents(calendarID, apiKey);
-        setEvents(events);
+        getEvents(process.env.REACT_APP_CALENDAR_ID, process.env.REACT_APP_API_KEY)
       }, []);
 
     return(
         <div>
-            UMMM
             {events?.map((event) => (
             <li key={event.id} >
                 {event.summary}
