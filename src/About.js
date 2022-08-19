185353a9b98a3e2732e6e1e1a6a592b9b850589d
Alina Zeng <alina.yzeng@gmail.com>
2022-08-18 22:30:24 -0400

    ADD new about page

--- src/About.js
+++ src/About.js
@@ -1,7 +1,43 @@
+import Banner from './Banner.js'
+import { Link } from 'react-router-dom';
+
 const About = () => {
     return(
         <div className="about">
-            Coming soon.
+            <Banner title="About Inspire Music" desc="Who we are – What we were built from – What we strive towards" id="about-banner" noButton={true} />
+            <section className="about-section  left-image">
+                <div className="about-thumb">
+                    <img src="https://scontent-yyz1-1.xx.fbcdn.net/v/t1.6435-9/40365447_852526034939550_7270469648817061888_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=UHFq4qL9A0YAX81CTki&_nc_ht=scontent-yyz1-1.xx&oh=00_AT-bJ-N2tQiF_E7mJd1hqJuMYCjYHQL2u_-jBHmRMcoDrg&oe=632409F4"></img>
+                </div>
+                <div className="about-content">
+                    <h1>Halfway through 2016,</h1>
+                    <p>Two individuals, deeply passionate about music and talented in unique ways, crossed paths. In next to no time, they became best friends. The idea of founding a professional, top-notch music school ignited a spark of purpose in both. After a year of meticulous planning and preparation, the school opened in December of 2017.</p>
+                    <p>Since then, we have reached far beyond our humble beginnings. Inspire Music Academy hosts a full range of talented music educators, a cohort of eager music students, and plenty of opportunities to become better.</p>
+                </div>
+            </section>
+            <section className="about-section right-image">
+                <div className="about-content">
+                    <h1>As part of our mission,</h1>
+                    <p>Alongside providing the sincere tools, instruction, and community needed to promote lifelong musical talent, we put emphasis on ensuring that our students become well-rounded.</p>
+                    <p>Our concerts, events, and camps, on top of regular lessons, enable our students to develop their planning abilities, public speaking skills, sense of teamwork, and ambition.</p>
+                </div>
+                <div className="about-thumb">
+                    <img src="https://scontent-yyz1-1.xx.fbcdn.net/v/t1.6435-9/43951867_872575202934633_4011884794634829824_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=MtluiI3FbLwAX-6DpSd&_nc_ht=scontent-yyz1-1.xx&oh=00_AT_OtjRF6o853GhIqx8vT68xythtjkiDNSTLShA7GHkSAQ&oe=63248D3F"></img>
+                </div>
+            </section>
+            <section className="about-section gallery-image">
+                <h1>Regardless of your current expertise,</h1>
+                <p>We're hear to support you in the next step of your music journey!</p>
+                <div className="gallery">
+                    <img src="https://scontent-yyz1-1.xx.fbcdn.net/v/t1.6435-9/43669679_872570702935083_817212614344966144_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=3Hq3YJehohoAX-VhPQD&tn=BA-zpYYrH1cox7WW&_nc_ht=scontent-yyz1-1.xx&oh=00_AT-CGp1gywZkkiN3KdAiAiyqLkytMDhwkSjcp8QuCNm2vQ&oe=63244BF2"></img>
+                    <img src="https://scontent-yyz1-1.xx.fbcdn.net/v/t1.6435-9/60338440_992977187561100_4551262713405243392_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=ljegikjft2MAX9AmD2a&_nc_ht=scontent-yyz1-1.xx&oh=00_AT-I38CnJJHxvrSsglADFX7LRnz308pwy3FFPx1KLm4Clw&oe=6325958A"></img>
+                    <img src="https://scontent-yyz1-1.xx.fbcdn.net/v/t1.6435-9/60325273_992977130894439_3648326653525884928_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=60AzhZr7IDQAX_OzRoj&_nc_ht=scontent-yyz1-1.xx&oh=00_AT_JMYLH8hPcbcX69eYA4om98iudcr-HsaS715bFpPxGHg&oe=63250743"></img>
+                    <img src="https://scontent-yyz1-1.xx.fbcdn.net/v/t1.6435-9/59987037_992977177561101_2110347436613435392_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=jzgLXbfYA-oAX8M-68o&_nc_ht=scontent-yyz1-1.xx&oh=00_AT9j3USANx1vqB6wVNj8QjlvC-EXPK28_jXNE3Pp5o4P-g&oe=63249189"></img>
+                    <img src="https://scontent-yyz1-1.xx.fbcdn.net/v/t1.6435-9/45020371_880134145512072_7955011659691458560_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=oOuV0Ciq8v0AX96UlGo&_nc_ht=scontent-yyz1-1.xx&oh=00_AT_8YTUyaJIF8O3YgUW-V_AsPfztMBy1a6lkduj6WBTKDA&oe=6324CC09"></img>
+                    <img src="https://scontent-yyz1-1.xx.fbcdn.net/v/t1.6435-9/44926429_880134132178740_7864140816111894528_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=bcIkzMJdoLsAX9O1zV0&_nc_ht=scontent-yyz1-1.xx&oh=00_AT-SnM3UZxD0CuWQnC5COyLb6mU_1HWX_ELxqGI6TfjDgA&oe=6323BAC3"></img>
+                </div>
+                <Link to="/contact/book" className="free-trial">Book a free trial lesson today</Link>
+            </section>
         </div>
     )
 }
--- src/Contact.js
+++ src/Contact.js
@@ -12,8 +12,12 @@ const Contact = () => {
                     <p>{<FaPhoneSquare size={15}/>} (905) 534-2456</p>
                     <p>{<FaEnvelopeSquare size={15} />} info@inspiremusiclesson.com</p>
                     <i></i>
-                    <p>{<FaMapMarkerAlt size={15} />} 6-9980 Kennedy Rd, Markham, ON</p>
-                    <p>(Kylemore’s Shoppe on Angus Glen Plaza, across TD Bank)</p>
+                    <p>{<FaMapMarkerAlt size={15} />} 6-9980 Kennedy Rd, Markham, ON (Kylemore’s Shoppe on Angus Glen Plaza, across TD Bank)</p>
+                    <ul>
+                        <li>Monday to Friday: 3PM to 9PM</li>
+                        <li>Saturday: 8:30AM to 7PM</li>
+                        <li>Sunday: 10AM to 7PM</li>
+                    </ul>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2875.0425285245474!2d-79.3197941!3d43.8963859!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4d58974d54509%3A0xe7c3206129284a8d!2s6-9980%20Kennedy%20Rd%2C%20Markham%2C%20ON%20L6C%200M4!5e0!3m2!1sen!2sca!4v1660181998513!5m2!1sen!2sca" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </section>
                 <section className="contact-direct">
